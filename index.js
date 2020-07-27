00693df6d34e112b19c4d046ede1eddfb3924e77
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 18:14:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector (selector){
+  return document.querySelector(selector)
+}
 
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+     const ulList=document.querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<ulList.length; i++){
+    ulList[i].innerHTML = (parseInt(ulList[i].innerHTML)+n).toString();
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
